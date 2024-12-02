d9e4de496cf436663c5bcfde74b6724224fc5889
fraskind <fraskind@umich.edu>
2024-12-02 14:59:23 -0500

    almost done

--- script.js
+++ script.js
@@ -8,12 +8,28 @@ buttons.forEach(button => {
             .querySelector("[data-slides]")
 
         const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        // const currNextSlide
+
+        let newIndex = ([...slides.children].indexOf(activeSlide) + offset) % slides.children.length
         // Looping aspect
         if (newIndex < 0) newIndex = slides.children.length - 1
-        if (newIndex >= slides.children.length) newIndex = 0
+        // if (newIndex >= slides.children.length) newIndex = 0
 
         slides.children[newIndex].dataset.active = true
         delete activeSlide.dataset.active
+
+        // Update prev/next slides
+        prevIndex = (newIndex - 1) % slides.children.length;
+        if (prevIndex < 0) prevIndex = slides.children.length - 1;
+
+        nextIndex = (newIndex + 1) % slides.children.length;
+        
+        slides.children[prevIndex].dataset.prev = true;
+        slides.children[nextIndex].dataset.next = true;
+        // Remove prev and next from other slides
+        [...slides.children].forEach((slide, index) => {
+            if (index !== prevIndex) delete slide.dataset.prev;
+            if (index !== nextIndex) delete slide.dataset.next;
+        });
     })
 })
